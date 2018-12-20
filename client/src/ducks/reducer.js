aa273a240a11901c04b26a2c78951cf5b784defb
Annie Hall <anniemaesato@gmail.com>
2018-12-19 20:34:04 -0700

    fixed conflict again

--- client/src/ducks/reducer.js
+++ client/src/ducks/reducer.js
@@ -50,8 +50,8 @@ function reducer(state = initialState, action) {
       };
     case UPDATE_DATE:
       return { ...state, date: action.payload };
-    case UPDATE_TITLE:
-      return { ...state, title: action.payload };
+    // case UPDATE_TITLE:
+    //   return { ...state, title: action.payload };
     case UPDATE_NOTES:
       return { ...state, notes: action.payload };
     case UPDATE_TEXT_DETECT:
@@ -65,65 +65,4 @@ function reducer(state = initialState, action) {
   }
 }
 
-<<<<<<< Updated upstream
-// EXPORT FUNCTIONS HERE
-
-export function updateSelectedFiles(files) {
-  console.log(files);
-  return {
-    type: UPDATE_SELECTED_FILES,
-    payload: files
-  };
-}
-export function updateDate(date) {
-  return {
-    type: UPDATE_DATE,
-    payload: date
-  };
-}
-export function updateTitle(title) {
-  return {
-    type: UPDATE_TITLE,
-    payload: title
-  };
-}
-export function updateNotes(notes) {
-  return {
-    type: UPDATE_NOTES,
-    payload: notes
-  };
-}
-export function updateS3Urls(s3Urls) {
-  return {
-    type: UPDATE_S3_URLS,
-    payload: s3Urls
-  };
-}
-export function updateTextDetect(text) {
-  return {
-    type: UPDATE_TEXT_DETECT,
-    payload: text
-  };
-}
-export function updateFolder(folder) {
-  return {
-    type: UPDATE_FOLDER,
-    payload: folder
-  };
-}
-export function onFormSubmit(file) {
-  return {
-    type: ON_FORM_SUBMIT,
-    payload: axios
-      .post(BASE_URL + "/api/add/file", file)
-      //.post(BASE_URL + "/api/upload", { file })
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(err => console.log(err))
-  };
-}
-
-=======
->>>>>>> Stashed changes
 export default reducer;
