f919345bc00807f447a03a091e43abd45d325bf5
Leah Hullinger <leahhullinger@gmail.com>
2018-12-21 19:45:15 -0700

    modal

---
+++ client/src/components/Modal/Modal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Modal.module.css'
+
+
+export const Modal = ({}) => {
+  return (
+    <div className={StyleSheet. }>
+      <header />
+      <main></main>
+      <footer></footer>
+    </div>
+  )
+  }
+
+
